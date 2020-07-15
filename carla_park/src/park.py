19052f4622e300fa739ad699f6fdf763de579a13
francesco <vignif@gmail.com>
2020-07-15 23:17:20 +0200

    cleaned code

--- carla_park/src/park.py
+++ carla_park/src/park.py
@@ -4,7 +4,8 @@
 park.py implements a basic parking policy for autonomous cars based on geometric information
 This script spawns:
     -one ego vehicle in x=61.4, y=-7.62, z=0.05
-    -two vehicle inside the parking location a side of the ego vehicle respectively in x=60.4, y=-10.62, z=0.05 and x=47.0, y=-10.62, z=0.05
+    -two vehicle inside the parking location a side of the ego vehicle respectively in
+        x=60.4, y=-10.62, z=0.05 and x=47.0, y=-10.62, z=0.05
     -a camera attached to the ego vehicle for future sensor based parking policy
 
 all the vehicles are rotated of 180' in order to be correctly in line with the street
@@ -20,19 +21,20 @@ tested env:
 -carla 0.9.7
 
 """
-
-import rospy
-import carla
 import time
 import random
+import rospy
+import carla
 from carla_msgs.msg import CarlaWorldInfo
 
 
 invert_steering_point = -7.8
-parked_locations = [carla.Transform(carla.Location(x=60.4, y=-10.62, z=0.05), carla.Rotation(yaw=180)),
-                    carla.Transform(carla.Location(x=47.0, y=-10.62, z=0.05), carla.Rotation(yaw=180))]
+parked_locations = [
+    carla.Transform(carla.Location(x=60.4, y=-10.62, z=0.05), carla.Rotation(yaw=180)),
+    carla.Transform(carla.Location(x=47.0, y=-10.62, z=0.05), carla.Rotation(yaw=180))
+    ]
 
-class CarlaParkVehicle(object):
+class CarlaParkVehicle():
     """
     class responsable of:
         -spawning 3 vehicles of which one ego
@@ -81,28 +83,33 @@ class CarlaParkVehicle(object):
         function enables the ego vehicle to enter the parking spot also actuating on steering wheels
 
         todo:
-        -a step function is applied on the steering wheels it would be more realistic to actuate the steering
+        -a step function is applied on the steering wheels it would be more
+        realistic to actuate the steering
         wheels with an increasing(decreasing) function i.e steer += (-)0.1
         -above mentioned approach requires a more sofisticated approach i.e. sensor based
-        -last manoeuvre of the parking procedure (go forward for completing the parking) is based only
-        on temporal information, variability can be caused by server delays
+        -last manoeuvre of the parking procedure (go forward for completing the parking)
+        is based only on temporal information, variability can be caused by server delays
         """
         while True:
-            self.vehicle.apply_control(carla.VehicleControl(throttle=0.3, steer=0.6, brake=0.0, reverse=True))
+            self.vehicle.apply_control(
+                carla.VehicleControl(throttle=0.3, steer=0.6, brake=0.0, reverse=True))
             time.sleep(0.1)
             if self.vehicle.get_location().y < invert_steering_point:
                 break
         while self.vehicle.get_location().y < invert_steering_point:
-            self.vehicle.apply_control(carla.VehicleControl(throttle=0.2, steer=-0.6, brake=0.0, reverse=True))
+            self.vehicle.apply_control(
+                carla.VehicleControl(throttle=0.2, steer=-0.6, brake=0.0, reverse=True))
             time.sleep(0.1)
             if abs(self.vehicle.get_transform().rotation.yaw) > 180 - 2:
                 rospy.loginfo("in parking line")
-                self.vehicle.apply_control(carla.VehicleControl(throttle=0.0, steer=0.0, brake=1.0, reverse=True))
+                self.vehicle.apply_control(
+                    carla.VehicleControl(throttle=0.0, steer=0.0, brake=1.0, reverse=True))
                 break
         self.vehicle.apply_control(carla.VehicleControl(throttle=0.0, brake=1.0))
         time.sleep(0.5)
         rospy.loginfo("go forward to complete the parking")
-        self.vehicle.apply_control(carla.VehicleControl(throttle=0.5, steer=0.0, brake=0.0, reverse=False))
+        self.vehicle.apply_control(
+            carla.VehicleControl(throttle=0.5, steer=0.0, brake=0.0, reverse=False))
         time.sleep(1.2)
         self.vehicle.apply_control(carla.VehicleControl(throttle=0.0, brake=1.0))
         time.sleep(0.5)
@@ -127,9 +134,8 @@ class CarlaParkVehicle(object):
         rospy.loginfo("Waiting for CARLA world (topic: /carla/world_info)...")
         try:
             rospy.wait_for_message("/carla/world_info", CarlaWorldInfo, timeout=10.0)
-        except rospy.ROSException as e:
+        except rospy.ROSException:
             rospy.logerr("Timeout while waiting for world info!")
-            raise e
         rospy.loginfo("CARLA world available. Spawn ego vehicle...")
 
         self.move_to_init_parking()
